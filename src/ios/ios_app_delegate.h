862435aedac5666ebf0da9b66563569401be30cc
ann0see <20726856+ann0see@users.noreply.github.com>
2023-02-12 19:54:31 +0100

    Make branch deprecation more obvious

--- src/ios/ios_app_delegate.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-#include <AVFoundation/AVFoundation.h>
-
-@interface                                QIOSApplicationDelegate : UIResponder <UIApplicationDelegate>
-@property ( strong, nonatomic ) UIWindow* window;
-
-@end
